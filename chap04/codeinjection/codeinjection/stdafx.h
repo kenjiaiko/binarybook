fb380648b551c6743948f910eca84843b8bd7c9c
Shyujikou <shyujikou@Solaluna.local>
2015-07-26 23:31:27 +0800

    Translation chap04

---
+++ chap04/codeinjection/codeinjection/stdafx.h
@@ -0,0 +1,14 @@
+// stdafx.h : 这里包含標準系统引用文件的引用文件，
+// 以及需要经常引用且很少发生变化的项目专用引用文件。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: 程序额外需要的头文件放在这里
